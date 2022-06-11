4d8bd8ccbcd98feea89f7620043d620cc91cda56
mjpc13 <mjpc13@protonmail.com>
2022-06-11 17:10:32 +0100

    First commit

---
+++ src/lib.rs
@@ -0,0 +1,31 @@
+use std::error::Error;
+use std::env;
+
+pub struct Config{
+    pub stack: i32
+}
+
+impl Config {
+    pub fn new(args: &[String]) -> Result<Config, &'static str> {
+
+        let arg = match args.len() {
+            1 => String::from("3"),
+            2 => args[1].clone(),
+            _ => panic!("Too many arguments to parse")
+        };
+
+        let stacks = arg.parse();
+        let stacks = match stacks {
+            Ok(stack) => Ok(Config{stack}),
+            Err(error) => Err("Error parsing the input"),
+        };
+
+        stacks
+    }
+}
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    
+    println!("Hanoi tower with {} stacks", config.stack);
+    Ok(())
+}
---
+++ src/main.rs
@@ -0,0 +1,32 @@
+use std::env;
+use std::process;
+
+use hanoi_tower::Config;
+
+/*  Process to keep your code organized:
+ *      - Split your program into a main.rs and a lib.rs and move your program's logic to lib.rs
+ *      - As long as your command line parsing logic is small, it can remain in main.rs
+ *      - When the command line parsing logic starts getting complicated, exctract it from main.rs and
+ *        move it to lib.rs
+ *
+ *
+ *  The main.rs responsabilities should be:
+ *      - Calling the command parsing logic with the arguments values
+ *      - Setting up any other configuration
+ *      - Calling a run function in lib.rs
+ *      - Handling the error if run returns an error
+ */
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("{:?}", err);
+        process::exit(1);
+    });
+
+    if let Err(_e) = hanoi_tower::run(config) {
+        process::exit(1);
+    };
+    
+}
